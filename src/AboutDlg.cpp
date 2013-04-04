cd4e5d965fd80817ad0deb7b419bee682f223a3f
Sergey Semushin <predelnik@gmail.com>
2013-04-05 00:54:54 +0400

    Add version number and link to github page to about dialog.

--- src/AboutDlg.cpp
+++ src/AboutDlg.cpp
@@ -20,6 +20,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <CommCtrl.h>
 
 #include "AboutDlg.h"
+#include "CommonFunctions.h"
+#include "MainDef.h"
+#include "Plugin.h"
 #include "resource.h"
 
 void AboutDlg::DoDialog ()
@@ -32,6 +35,54 @@ void AboutDlg::DoDialog ()
   display ();
 }
 
+bool GetProductAndVersion (TCHAR *&strProductVersion)
+{
+  // get the filename of the executable containing the version resource
+  TCHAR szFilename[MAX_PATH + 1] = {0};
+  if (GetModuleFileName ((HMODULE) getHModule (), szFilename, MAX_PATH) == 0)
+  {
+    return false;
+  }
+
+  // allocate a block of memory for the version info
+  DWORD dummy;
+  DWORD dwSize = GetFileVersionInfoSize(szFilename, &dummy);
+  if (dwSize == 0)
+  {
+    return false;
+  }
+  std::vector<BYTE> data(dwSize);
+
+  // load the version info
+  if (!GetFileVersionInfo(szFilename, NULL, dwSize, &data[0]))
+  {
+    return false;
+  }
+
+  // get the name and version strings
+  LPVOID pvProductName = NULL;
+  unsigned int iProductNameLen = 0;
+  LPVOID pvProductVersion = NULL;
+  unsigned int iProductVersionLen = 0;
+
+  UINT                uiVerLen = 0;
+  VS_FIXEDFILEINFO*   pFixedInfo = 0;     // pointer to fixed file info structure
+  // get the fixed file info (language-independent)
+  if(VerQueryValue(&data[0], TEXT("\\"), (void**)&pFixedInfo, (UINT *)&uiVerLen) == 0)
+  {
+    return false;
+  }
+
+  CLEAN_AND_ZERO_ARR (strProductVersion);
+  strProductVersion = new TCHAR[DEFAULT_BUF_SIZE];
+  _stprintf (strProductVersion, _T ("Version: %u.%u.%u.%u"),     HIWORD (pFixedInfo->dwProductVersionMS),
+    LOWORD (pFixedInfo->dwProductVersionMS),
+    HIWORD (pFixedInfo->dwProductVersionLS),
+    LOWORD (pFixedInfo->dwProductVersionLS));
+
+  return true;
+}
+
 void AboutDlg::init (HINSTANCE hInst, HWND Parent)
 {
   return Window::init (hInst, Parent);
@@ -42,6 +93,12 @@ BOOL CALLBACK AboutDlg::run_dlgProc (UINT message, WPARAM wParam, LPARAM lParam)
   switch (message)
   {
   case WM_INITDIALOG:
+    {
+      TCHAR *Ver = 0;
+      GetProductAndVersion (Ver);
+      Static_SetText (::GetDlgItem(_hSelf, IDC_VERSION), Ver);
+      CLEAN_AND_ZERO_ARR (Ver);
+    }
     return TRUE;
   case WM_NOTIFY:
     {
