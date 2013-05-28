73934af9741f2e1a4ce7d158470cb8739e1e3647
Sergey Semushin <predelnik@gmail.com>
2013-05-28 13:43:31 +0400

    If scroll was made 'cause user input, don't recheck.

--- src/DllMain.cpp
+++ src/DllMain.cpp
@@ -246,7 +246,7 @@ extern "C" __declspec(dllexport) void beNotified(SCNotification *notifyCode)
     break;
 
   case SCN_UPDATEUI:
-    if(notifyCode->updated & (SC_UPDATE_V_SCROLL | SC_UPDATE_H_SCROLL))
+    if(notifyCode->updated & (SC_UPDATE_V_SCROLL | SC_UPDATE_H_SCROLL) && !Timer) // If scroll wasn't caused by user input...
     {
       SendEvent (EID_RECHECK_INTERSECTION);
     }
