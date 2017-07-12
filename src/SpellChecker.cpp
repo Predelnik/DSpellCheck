f0d96557a02eb78ed6aeb37c1bca7a48bea25fd4
Christian Grasser <christian.grasser@live.de>
2017-07-13 00:03:49 +0200

    initial appveyor.yml CI config

--- src/SpellChecker.cpp
+++ src/SpellChecker.cpp
@@ -23,7 +23,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "Aspell.h"
 #include "DownloadDicsDlg.h"
-#include "iconv.h"
 #include "CommonFunctions.h"
 #include "LanguageName.h"
 #include "LangList.h"
