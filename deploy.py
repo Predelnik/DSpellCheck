b15d7a502b782e330b9e88f0c5c3d6277bf1096d
Sergey Semushin <predelnik@gmail.com>
2017-11-25 12:44:29 +0300

    Automatically change npp-plugins-x64 sources. (incomplete)

--- deploy.py
+++ deploy.py
@@ -7,6 +7,7 @@ import argparse
 import re
 import sys
 import shutil
+from bs4 import BeautifulSoup
 
 def get_version_number (filename):
 	info = GetFileVersionInfo (filename, "\\")
@@ -62,6 +63,7 @@ if options.new_minor:
 	print ('Version increased to {}'.format ('.'.join (ver)))
 
 for arch in ['x64', 'x86']:
+	'''
 	dir = 'build-deploy-msvc2017-{}'.format (arch)
 	build_args = ['cmake', '--build', dir, '--config', 'RelWithDebInfo']
 	FNULL = open(os.devnull, 'w')
@@ -85,6 +87,23 @@ for arch in ['x64', 'x86']:
 	zip (binary_path, target_zip_path)
 	print ('Copying PDB...')
 	shutil.copy (pdb_path, target_pdb_path)
+	'''
+if 'NPP_PLUGINS_X64_PATH' in os.environ:
+	plugins_x64_path = os.environ['NPP_PLUGINS_X64_PATH']
+	print ('Applying change to npp-plugins-x64 source directory...')
+	plugins_xml_path = os.path.join (plugins_x64_path, 'plugins/plugins64.xml')
+	plugins_xml_data = ''
+	with open (plugins_xml_path, "r") as file:
+		plugins_xml_data = file.read()
+	bs = BeautifulSoup(plugins_xml_data, features="xml")
+	plugin_node = bs.plugins.find("plugin", attrs={'name' : 'DSpellCheck'})
+	ver = get_rc_version ()
+	plugin_node.x64Version.string = ver
+	plugin_node.download.string = 'https://github.com/Predelnik/DSpellCheck/releases/download/v{}/DSpellCheck_x64.zip'.arg (ver);
+	print (plugin_node)
+else:
+	print ('%NPP_PLUGINS_X64_PATH% is not set up, nothing to update')
+
 successString = 'Success!'
 try:
 	from colorama import init, Fore, Style
