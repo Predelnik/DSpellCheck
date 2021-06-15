693dc0f4a21ddb55d850e4a146aa1e6b3fa24509
Sergey Semushin <predelnik@gmail.com>
2021-06-15 20:40:40 +0300

    Add changelog link to about dialog

--- src/plugin/resource.h
+++ src/plugin/resource.h
@@ -44,6 +44,7 @@
 #define IDC_ONE_USER_DIC                1008
 #define IDC_USERNAME                    1008
 #define IDD_ADVANCED                    1008
+#define IDC_SYSLINK4                    1008
 #define IDC_ADDRESS                     1009
 #define IDC_PORT                        1009
 #define IDD_SETTINGS                    1009
@@ -222,7 +223,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1008
+#define _APS_NEXT_CONTROL_VALUE         1009
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
