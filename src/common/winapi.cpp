60d75ff9ab444aa423fd736289a86a23394838f9
Sergey Semushin <predelnik@gmail.com>
2023-08-20 16:29:31 +0200

    Remove unnecessary CoUnitialize

--- src/common/winapi.cpp
+++ src/common/winapi.cpp
@@ -94,8 +94,6 @@ std::optional<std::wstring> browse_for_directory(HWND parent_wnd, const wchar_t 
       ret = sz_path.data();
     CoTaskMemFree(pidl);
     // free memory used
-
-    CoUninitialize();
   }
   return ret;
 }
